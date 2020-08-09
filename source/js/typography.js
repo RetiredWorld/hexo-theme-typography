f1d4b6b608443320c993ff7c4d551130dfce24f8
zeka <2667286947@qq.com>
2020-08-09 22:27:06 +0800

    fix resize bug

--- source/js/typography.js
+++ source/js/typography.js
@@ -1,16 +1,6 @@
 var stage;
 var siteNavShown = true;
 
-function triggerSiteNav() {
-    return;
-    if (siteNavShown) {
-        $('#site-nav').hide(300);
-        siteNavShown = false;
-    } else {
-        $('#site-nav').show(300);
-        siteNavShown = true;
-    }
-}
 function updateSidebar() {
     if (window.innerWidth <= 768 || window.innerHeight <= 600) {
         $('#side-bar').innerWidth($('#stage').width());
@@ -20,11 +10,11 @@ function updateSidebar() {
     } else {
         //$('#site-nav').show();
         //siteNavShown = true;
+        $('#main-container').addClass('col-sm-9');
         var sidebarW =
             stage.width() - $('#main-container').outerWidth() + (window.innerWidth - stage.innerWidth()) / 2;
         $('#side-bar').outerWidth(sidebarW);
         console.log("sidebarW=" + sidebarW);
-        $('#main-container').addClass('col-sm-9');
     }
 }
 $(document).ready(function () {
